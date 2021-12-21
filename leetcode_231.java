c32f72230bf9270ec17eb63e96f1ec26a842da23
Adios <30585897+jongwanS@users.noreply.github.com>
2021-12-21 19:16:50 +0900

    Create leetcode_231.java

---
+++ leetcode_231.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        //음수나, 0일경우 표현못함
+        if(n <= 0) return false;
+        
+        int cnt = 0;
+		while(n >= 1) {
+			if((n & 1) == 1) {
+                //n과 1을 and 연산을 통해 1의 갯수를 센다.
+				cnt++;
+			}
+			n = n >> 1;//쉬프트 연산 (오른쪽으로 1번 이동)
+            if(cnt>1) return false; //2의 제곱일경우 1의 갯수가 1개여야함
+		}
+		
+		return true;
+    }
+}
