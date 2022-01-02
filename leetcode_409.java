5b684977a255a7518228c67ac0dee381565527de
Adios <30585897+jongwanS@users.noreply.github.com>
2022-01-02 18:32:23 +0900

    Create leetcode_409.java

---
+++ leetcode_409.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        
+        Map<Character, Integer> map = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch,0) +1);
+        }
+        
+        int res = 0;
+        boolean oddYn = false;
+        for(char key : map.keySet()){
+            int n = map.get(key);
+            if(n%2 == 0){
+                res+=n;
+            }else{
+                res+=n-1;                
+                oddYn = true;
+            }
+        }
+        if(oddYn){
+            res++;
+        }
+        return res;
+    }
+}
