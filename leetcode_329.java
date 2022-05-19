44a2e6177606c98bc1154ed196e2077b752bcca8
Adios <30585897+jongwanS@users.noreply.github.com>
2022-05-20 01:26:00 +0900

    Create leetcode_329.java

---
+++ leetcode_329.java
@@ -0,0 +1,42 @@
+package leetcode_github;
+
+class Solution {
+    
+    int[][] directions = { {1,0},{-1,0},{0,1},{0,-1} };
+    
+    public int longestIncreasingPath(int[][] matrix) {
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        int[][] cache = new int[n][m];
+        
+        int longestPath = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int longest = longestIncreasingPath(matrix, cache, n, m, i, j);
+                longestPath = Math.max(longestPath, longest);
+            }
+        }
+        
+        return longestPath;
+    }
+    
+    private int longestIncreasingPath(int[][] matrix, int[][] cache, int n, int m, int i, int j){
+        if(cache[i][j] > 0) return cache[i][j];
+        
+        int max = 0;
+        for(int[] direction : directions){
+            int x = direction[0] + i;
+            int y = direction[1] + j;
+            
+            if(x > -1 && y > -1 && x < n && y < m && matrix[x][y] > matrix[i][j]){
+                int longest = longestIncreasingPath(matrix, cache, n, m, x, y);
+                max = Math.max(max, longest);
+            }
+        }
+        
+        cache[i][j] = max+1;
+        return cache[i][j];
+    }
+}
