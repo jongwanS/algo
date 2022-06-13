1e6e5714da853e512fa98e860408f5593aad3742
Adios <30585897+jongwanS@users.noreply.github.com>
2022-06-13 23:42:33 +0900

    Create leetcode_120.java

---
+++ leetcode_120.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        /**
+           2
+          3 4
+         6 5 7
+        4 1 8 3
+        */
+        
+        for(int i=triangle.size()-1;i>=0;i--){
+            for(int j=0;j<triangle.get(i).size()-1;j++){
+                int before = triangle.get(i).get(j);
+                int after = triangle.get(i).get(j+1);
+                triangle.get(i-1).set(j, triangle.get(i-1).get(j)+ Math.min(before,after));
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}
