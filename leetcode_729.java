bb9fe6707248612cfaaa65559425945cfb6fa0a8
Adios <30585897+jongwanS@users.noreply.github.com>
2022-08-03 22:18:06 +0900

    [leetcode] treemap - floorKey - ceilingKey

---
+++ leetcode_729.java
@@ -0,0 +1,23 @@
+class MyCalendar {
+    TreeMap<Integer, Integer> tree;
+    public MyCalendar() {
+        this.tree = new TreeMap<>();
+    }
+
+    public boolean book(int start, int end) {
+
+        Integer fKey = tree.floorKey(start);//시작 보다 작은 첫번째(key 자기자신 포함, 없으면 null)
+        Integer cKey = tree.ceilingKey(start);//시작 보다 큰 첫번째(key 자기자신 포함, 없으면 null)
+        if(fKey != null && tree.get(fKey) > start) return false;
+        if(cKey != null && cKey < end) return false;
+
+        tree.put(start, end);
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
