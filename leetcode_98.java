bab9801156ef9b615b826f2a86c52fe8e387f20c
Adios <30585897+jongwanS@users.noreply.github.com>
2022-08-12 00:00:07 +0900

    [leetcode] 이진탐색트리 - 이진탐색트리의 parent는 왼쪽 자식보다 무조건 크고   오른쪽 자식보다는 무조건 커야함. - inorder traversal 방식으로 list 넣고 정렬순인지확인

---
+++ leetcode_98.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        ArrayList<Integer>list = new ArrayList<>();
+        inOrder(list,root);
+        int n = list.size();
+        for(int i=0;i<n-1;i++){
+            if(list.get(i)>=list.get(i+1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void inOrder(ArrayList<Integer>list,TreeNode root){
+        if(root==null){
+            return;
+        }
+        inOrder(list,root.left);
+        list.add(root.val);
+        inOrder(list,root.right);
+    }
+}
