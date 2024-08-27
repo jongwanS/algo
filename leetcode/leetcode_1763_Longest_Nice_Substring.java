2a0ec34c5837ce6cb1a5ac0813133c84b15a2bf2
limjong-wan <lim8848@gmail.com>
2024-08-28 00:11:30 +0900

    leetcode_MaximumAverageSubarrayI_643

---
+++ leetcode/leetcode_1763_Longest_Nice_Substring.java
@@ -0,0 +1,15 @@
+package leetcode;
+//https://leetcode.com/problems/longest-nice-substring/description/
+public class leetcode_1763_Longest_Nice_Substring {
+    static class Solution {
+        public String longestNiceSubstring(String s) {
+            return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "YazaAay";
+        solution.longestNiceSubstring(s);
+    }
+}
